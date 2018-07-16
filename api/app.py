a6e6c3996556059b892b6ff2cc382fede7c3114b
JomaryDaison16 <jomari.daison@gmail.com>
2018-07-16 10:54:43 +0800

    fixing login4

--- api/app.py
+++ api/app.py
@@ -18,7 +18,7 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-  return Admin.query.get(user_id)
+  return Admin.query.get(int(user_id)).first()
 
 @app.route('/', methods=['GET'])
 def admin_create():
