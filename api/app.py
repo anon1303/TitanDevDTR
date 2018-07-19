0806c96c60be34b5876df14c6a7d2ccf8e6be957
JomaryDaison16 <jomari.daison@gmail.com>
2018-07-19 10:17:04 +0800

    debugging2

--- api/app.py
+++ api/app.py
@@ -413,7 +413,7 @@ def employee_week(dates, emp_id):
    year, week_number = dates.split("-")
    print year
    print week_number
-   summary = Attendance.query.filter(Attendance.employeeid == emp_id).filter(Attendance.week_number = week_number).filter(extract('year', Attendance.date) == year).all()
+   summary = Attendance.query.filter(Attendance.employeeid == emp_id).filter(Attendance.week_number == week_number).filter(extract('year', Attendance.date) == year).all()
    employees = []
    if summary is None:
        return jsonify({"message": "No data to show"})
