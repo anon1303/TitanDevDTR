ae9dc3620eadbca89382a32f9e337b9831c64b81
charlinB <nicolebugas@gmail.com>
2018-07-20 09:48:44 +0800

    new/old designs

--- www/js/index.js
+++ www/js/index.js
@@ -1,68 +1,46 @@
-function scanner(){
-    //Prepare
-    var done = function(err, status){
-      if(err){
-        console.error(err._message);
-      } else {
-        console.log('QRScanner is initialized. Status:');
-        console.log(status);
-      }
-    };
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
 
-    QRScanner.prepare(done);
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
 
-    //Scan
-    var callback = function(err, contents){
-      if(err){
-        console.error(err._message);
-      }
-      alert('The QR Code contains: ' + contents);
-    };
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-    QRScanner.scan(callback);
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-    //Cancel
-    QRScanner.cancelScan(function(status){
-      console.log(status);
-    });
+        console.log('Received Event: ' + id);
+    }
+};
 
-    //Show
-    QRScanner.show(function(status){
-      console.log(status);
-    });
-
-    //Hide
-    QRScanner.hide(function(status){
-      console.log(status);
-    });
-
-    //Lighting
-    QRScanner.enableLight(function(err, status){
-      err && console.error(err);
-      console.log(status);
-    });
-
-    //Disable light
-    QRScanner.disableLight(function(err, status){
-      err && console.error(err);
-      console.log(status);
-    });
-
-    //Camera Reversal
-    QRScanner.useFrontCamera(function(err, status){
-      err && console.error(err);
-      console.log(status);
-    });
-
-    QRScanner.useBackCamera(function(err, status){
-      err && console.error(err);
-      console.log(status);
-    });
-
-    var back = 0; // default camera on plugin initialization
-    var front = 1;
-    QRScanner.useCamera(front, function(err, status){
-      err && console.error(err);
-      console.log(status);
-    });
-}
+app.initialize();
