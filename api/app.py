95a6450cdf8fb2a27e79c4f46643a643ef8d5c2d
JomaryDaison16 <jomari.daison@gmail.com>
2018-07-20 10:13:21 +0800

    changed models attendance date to date type added extra conversion for time in.

--- api/app.py
+++ api/app.py
@@ -501,7 +501,8 @@ def edit_time():
 @app.route('/TimeIn/', methods=['POST'])
 def timein():
     now = datetime.now().strftime("%m%d%Y%H%M")
-    datenow = datetime.now().strftime("%m%d%Y")
+    datenow1 = datetime.now().strftime("%m%d%Y")
+    datenow = datetime.strptime(str(datenow1), "%m%d%Y")
     timeAdmin = Admin.query.get(1)
 
     morning7 = timeAdmin.morning_time_in_start.strftime("%H%M")
